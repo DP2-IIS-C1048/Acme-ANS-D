1742668eafe9e9ce02aa7df5a32fc99bc70505ff
rafaelroodrgz <rafaelromu24@gmail.com>
2025-02-28 12:54:43 +0100

    feat(TrackingLog): Se ha añadido la entidad TrackingLog.
    Añadida una clase TrackingLog y un enum
    TrackingLogIndicator.

---
+++ src/main/java/acme/entities/tracking_log/TrackingLog.java
@@ -0,0 +1,44 @@
+
+package acme.entities.tracking_log;
+
+import java.util.Date;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Past;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+
+public class TrackingLog extends AbstractEntity {
+
+	private static final long		serialVersionUID	= 1L;
+
+	@Mandatory
+	@Past()
+	@Automapped
+	private Date					lastUpdateMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String					step;
+
+	@Mandatory
+	@ValidScore
+	@Automapped
+	private Double					resolutionPercentage;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TrackingLogIndicator	indicator;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String					resolution;
+
+}
