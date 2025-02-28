94096cdf9030f3b83e464831381b59506753c29d
DavidBBSA <davbasbra@alum.us.es>
2025-02-28 14:51:50 +0100

    Añadido la entidad FlightAssignment
    Se ha creado una nueva entidad que representa la asignación del vuelo.
    
    Refs  #38

---
+++ src/main/java/acme/entities/flight_assignment/FlightAssignment.java
@@ -0,0 +1,54 @@
+
+package acme.entities.flight_assignment;
+
+import java.util.Date;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+
+public class FlightAssignment extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Duty				duty;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				lastUpdate;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private CurrentStatus		currentStatus;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				remarks;
+
+	/*
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private FlightCrewMember	flightCrewMember;
+
+	@Mandatory
+	@Valid
+	@OneToOne
+	private Leg					leg;
+	*/
+}
