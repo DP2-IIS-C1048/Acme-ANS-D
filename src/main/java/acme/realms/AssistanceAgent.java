d76ef7d26f72d6b107ef8b44909715eb822beb34
rafaelroodrgz <rafaelromu24@gmail.com>
2025-02-28 11:39:03 +0100

    Role AssitanceAgent editado:
    Mejoradas las validaciones de la clase y cambiada la extensión a
    AbstractRole

---
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -0,0 +1,63 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.Past;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AssistanceAgent extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				spokenLanguages;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private String				airline;
+
+	@Mandatory
+	@Past()
+	@Automapped
+	private Date				moment;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				briefBio;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	@Optional
+	@Valid
+	@Automapped
+	private String				photo;
+
+}
