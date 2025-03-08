1cabbe11a9e2cadd1b6b756e9a6f0f5bc6156660
DavidBBSA <davbasbra@alum.us.es>
2025-03-08 23:02:49 +0100

    Corregido la entidad FlightAssignment
    Se añadido algunas anotaciones a FlightAssignment necesarias.

--- src/main/java/acme/entities/flight_assignment/FlightAssignment.java
+++ src/main/java/acme/entities/flight_assignment/FlightAssignment.java
@@ -3,6 +3,7 @@ package acme.entities.flight_assignment;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -15,7 +16,12 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class FlightAssignment extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
