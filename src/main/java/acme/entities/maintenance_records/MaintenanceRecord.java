2b23b2b7b4d171beb838c8933c365cc06d8fa19a
Marco2026 <marherluj@alum.us.es>
2025-03-09 14:11:15 +0100

    Added commented relationship with involves class

--- src/main/java/acme/entities/maintenance_records/MaintenanceRecord.java
+++ src/main/java/acme/entities/maintenance_records/MaintenanceRecord.java
@@ -3,11 +3,11 @@ package acme.entities.maintenance_records;
 
 import java.util.Date;
 
-import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
@@ -24,30 +24,36 @@ public class MaintenanceRecord extends AbstractEntity {
 
 	@Mandatory
 	@ValidMoment
-	private Date				moment;
+	@Automapped
+	private Date				maintenanceMoment;
 
 	@Mandatory
 	@Valid
+	@Automapped
 	private MaintenanceStatus	status;
 
 	@Mandatory
 	@ValidMoment
+	@Automapped
 	private Date				inspectionDueDate;
 
 	@Mandatory
 	@ValidMoney
+	@Automapped
 	private Money				estimatedCost;
 
 	@Optional
 	@ValidString
+	@Automapped
 	private String				notes;
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
 
-	@Mandatory
-	@Valid
-	@ManyToOne(optional = false)
-	private Aircraft			aircraft;
+	//	@Mandatory
+	//	@Valid
+	//	@ManyToOne(optional = false)
+	//	private Aircraft			aircraft;
+
 }
