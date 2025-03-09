01c40dfe702bd0865474f9f04f9d03b5f25a2d54
guigonvaz <guigonvaz@alum.us.es>
2025-03-09 11:33:30 +0100

    Customer implementado con validador

--- src/main/java/acme/entities/customer/Customer.java
+++ src/main/java/acme/entities/customer/Customer.java
@@ -10,12 +10,14 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidCustomer;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidCustomer
 public class Customer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
@@ -31,22 +33,22 @@ public class Customer extends AbstractRole {
 	private String				phoneNumber;
 
 	@Mandatory
-	@ValidString(max = 255, min = 1)
+	@ValidString(max = 255)
 	@Automapped
 	private String				physicalAddress;
 
 	@Mandatory
-	@ValidString(max = 50, min = 1)
+	@ValidString(max = 50)
 	@Automapped
 	private String				city;
 
 	@Mandatory
-	@ValidString(max = 50, min = 1)
+	@ValidString(max = 50)
 	@Automapped
 	private String				country;
 
 	@Optional
-	@ValidNumber(max = 50000, min = 0)
+	@ValidNumber(max = 50000)
 	@Automapped
 	private Integer				earnedPionts;
 
