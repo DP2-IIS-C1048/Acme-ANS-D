bdd740d1ed5847be8ca36c4f04babf3f582ccf9b
DavidBBSA <davbasbra@alum.us.es>
2025-03-10 22:47:52 +0100

    Corregida la relación entre flighAssignment y Leg

--- src/main/java/acme/entities/flight_assignment/FlightAssignment.java
+++ src/main/java/acme/entities/flight_assignment/FlightAssignment.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -53,7 +52,7 @@ public class FlightAssignment extends AbstractEntity {
 
 	@Mandatory
 	@Valid
-	@OneToOne
+	@ManyToOne
 	private Leg					leg;
 
 }
