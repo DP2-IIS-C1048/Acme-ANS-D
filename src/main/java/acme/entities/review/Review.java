f394de097e52b2ab0240b2d861b4b79064d6e7c2
DAVID CHAVES <davchacum@alum.us.es>
2025-03-10 09:42:10 +0100

    fix(Review): Se ha eliminado ValidReview y se usa ahora validNumber

--- src/main/java/acme/entities/review/Review.java
+++ src/main/java/acme/entities/review/Review.java
@@ -12,8 +12,8 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
-import acme.constraints.ValidReviewScore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -45,7 +45,7 @@ public class Review extends AbstractEntity {
 	private String				text;
 
 	@Optional
-	@ValidReviewScore()
+	@ValidNumber(min = 0, max = 10, integer = 2, fraction = 2)
 	@Automapped
 	private Double				score;
 
