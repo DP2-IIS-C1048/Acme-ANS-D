a94cdec4cbf41eebe72304a2925ec169864e6886
DAVID CHAVES <davchacum@alum.us.es>
2025-03-12 10:12:35 +0100

    feat(Leg): Se añadió nuevas validaciones y se actualizaron los mensajes de error

--- src/main/java/acme/entities/booking/Booking.java
+++ src/main/java/acme/entities/booking/Booking.java
@@ -1,6 +1,8 @@
 
 package acme.entities.booking;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -9,7 +11,6 @@ import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.datatypes.Moment;
 import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
@@ -39,7 +40,7 @@ public class Booking extends AbstractEntity {
 	@Mandatory
 	@ValidMoment(past = true, min = "2000/01/01 00:00")
 	@Temporal(TemporalType.TIMESTAMP)
-	private Moment				purchaseMoment;
+	private Date				purchaseMoment;
 
 	@Mandatory
 	@Valid
