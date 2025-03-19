402a7dc7dc0e790c5178e88760cc9fd505cc4d5e
guigonvaz <guigonvaz@alum.us.es>
2025-03-19 10:23:52 +0100

    Actualizado error en el patrón de identifier

--- src/main/java/acme/realms/customer/Customer.java
+++ src/main/java/acme/realms/customer/Customer.java
@@ -23,7 +23,7 @@ public class Customer extends AbstractRole {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
 	@Column(unique = true)
 	private String				identifier;
 
