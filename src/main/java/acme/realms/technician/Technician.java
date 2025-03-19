b293bd3bc47195770503dc0919e8ffd58a911b79
Marco2026 <marherluj@alum.us.es>
2025-03-19 20:46:00 +0100

    fix: forgot to remove min and max in technician license

--- src/main/java/acme/realms/technician/Technician.java
+++ src/main/java/acme/realms/technician/Technician.java
@@ -28,7 +28,7 @@ public class Technician extends AbstractRole {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
 	@Column(unique = true)
 	private String				license;
 
