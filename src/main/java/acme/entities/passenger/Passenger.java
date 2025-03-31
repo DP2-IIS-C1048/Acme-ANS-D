f59effc77a9cc3e53f18f19e4f0750854afecf91
guigonvaz <guigonvaz@alum.us.es>
2025-03-31 11:15:14 +0200

    Añadido atributo draftMode a Passenger para cumplir el requisito

--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -48,4 +49,9 @@ public class Passenger extends AbstractEntity {
 	@ValidString(max = 50)
 	@Automapped
 	private String				specialNeeds;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				draftMode;
 }
