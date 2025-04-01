81d0e6e1b8cf7c6ecc4e2d1f8f74280d33776676
guigonvaz <guigonvaz@alum.us.es>
2025-04-01 18:21:41 +0200

    Arregladas Querys del repositorio acorde a la nueva relación

--- src/main/java/acme/features/authenticated/customer/passenger/CustomerPassengerRepository.java
+++ src/main/java/acme/features/authenticated/customer/passenger/CustomerPassengerRepository.java
@@ -13,12 +13,12 @@ import acme.realms.customer.Customer;
 @Repository
 public interface CustomerPassengerRepository extends AbstractRepository {
 
-	@Query("Select p from Passenger p join p.bookingRecords br where br.booking.customer.id = :customerId")
+	@Query("Select p from Passenger p where p.customer.id = :customerId")
 	List<Passenger> findPassengersByCustomerId(int customerId);
 
 	@Query("Select p from Passenger p where p.id = :passengerId")
 	Passenger findPassengerById(int passengerId);
 
-	@Query("select br.booking.customer from BookingRecord br where br.passenger.id = :passengerId")
+	@Query("select p.customer from Passenger p where p.id = :passengerId")
 	Customer findCustomerByPassengerId(int passengerId);
 }
