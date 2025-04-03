afcea08ba0d6aaf91959cdf91121590f1dfb379f
Marco2026 <marherluj@alum.us.es>
2025-04-03 20:04:42 +0200

    Uploaded necessary classes for technician in frontend

---
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordListService.java
@@ -0,0 +1,49 @@
+
+package acme.features.technician.maintenanceRecord;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.maintenance.MaintenanceRecord;
+import acme.realms.technician.Technician;
+
+@GuiService
+public class TechnicianMaintenanceRecordListService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<MaintenanceRecord> maintenanceRecords;
+
+		maintenanceRecords = this.repository.findAllMaintenanceRecords();
+
+		super.getBuffer().addData(maintenanceRecords);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord mainenanceRecord) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(mainenanceRecord, "title", "moment", "status");
+		super.addPayload(dataset, mainenanceRecord, "text");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
---
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.technician.maintenanceRecord;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.maintenance.MaintenanceRecord;
+
+@Repository
+public interface TechnicianMaintenanceRecordRepository extends AbstractRepository {
+
+	@Query("select mr from MaintenanceRecord mr where m.id = :id")
+	MaintenanceRecord findMaintenanceRecordById(int id);
+
+	@Query("select mr from MaintenanceRecord mr")
+	Collection<MaintenanceRecord> findAllMaintenanceRecords();
+
+}
