dfc2c5f7b85ed7ecaa35faac2e8df667a7fffec2
Marco2026 <marherluj@alum.us.es>
2025-04-12 14:12:37 +0200

    Basic structure of the controller finished

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordController.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordController.java
@@ -1,6 +1,61 @@
+/*
+ * TechnicianLegController.java
+ *
+ * Copyright (C) 2012-2025 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
 package acme.features.technician.maintenanceRecord;
 
+import javax.annotation.PostConstruct;
 
-public class TechnicianMaintenanceRecordController {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.maintenance.MaintenanceRecord;
+import acme.realms.technician.Technician;
+
+@GuiController
+public class TechnicianMaintenanceRecordController extends AbstractGuiController<Technician, MaintenanceRecord> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private TechnicianMaintenanceRecordListService		listService;
+
+	@Autowired
+	private TechnicianMaintenanceRecordShowService		showService;
+
+	@Autowired
+	private TechnicianMaintenanceRecordCreateService	createService;
+
+	@Autowired
+	private TechnicianMaintenanceRecordUpdateService	updateService;
+
+	@Autowired
+	private TechnicianMaintenanceRecordDeleteService	deleteService;
+
+	@Autowired
+	private TechnicianMaintenanceRecordPublishService	publishService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+		super.addBasicCommand("create", this.createService);
+		super.addBasicCommand("update", this.updateService);
+		super.addBasicCommand("delete", this.deleteService);
+
+		super.addCustomCommand("publish", "update", this.publishService);
+	}
 
 }
