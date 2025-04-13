7d95f787649bab76f47112f0cec0cb7e61a91f75
Marco2026 <marherluj@alum.us.es>
2025-04-13 15:03:42 +0200

    update service basic structure finished

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordUpdateService.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordUpdateService.java
@@ -1,6 +1,80 @@
 
 package acme.features.technician.maintenanceRecord;
 
-public class TechnicianMaintenanceRecordUpdateService {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.components.ExchangeRate;
+import acme.entities.maintenance.MaintenanceRecord;
+import acme.realms.technician.Technician;
+
+@GuiService
+public class TechnicianMaintenanceRecordUpdateService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+
+	@Override
+	public void authorise() {
+
+		boolean status;
+		int maintenanceRecordId;
+		MaintenanceRecord maintenanceRecord;
+		Technician technician;
+
+		maintenanceRecordId = super.getRequest().getData("id", int.class);
+		maintenanceRecord = this.repository.findMaintenanceRecordById(maintenanceRecordId);
+		technician = maintenanceRecord == null ? null : maintenanceRecord.getTechnician();
+		status = maintenanceRecord != null && maintenanceRecord.isDraftMode() && super.getRequest().getPrincipal().hasRealm(technician);
+
+		super.getResponse().setAuthorised(status);
+
+	}
+
+	@Override
+	public void load() {
+		MaintenanceRecord maintenanceRecord;
+		int maintenanceRecordId;
+
+		maintenanceRecordId = super.getRequest().getData("id", int.class);
+		maintenanceRecord = this.repository.findMaintenanceRecordById(maintenanceRecordId);
+
+		super.getBuffer().addData(maintenanceRecord);
+	}
+
+	@Override
+	public void bind(final MaintenanceRecord maintenanceRecord) {
+		super.bindObject(maintenanceRecord, "maintenanceMoment", "status", "inspectionDueDate", "estimatedCost", "notes");
+	}
+
+	@Override
+	public void validate(final MaintenanceRecord maintenanceRecord) {
+		{
+			boolean validCurrency = ExchangeRate.isValidCurrency(maintenanceRecord.getEstimatedCost().getCurrency());
+			super.state(validCurrency, "estimatedCost", "acme.validation.currency.message");
+		}
+	}
+
+	@Override
+	public void perform(final MaintenanceRecord maintenanceRecord) {
+		this.repository.save(maintenanceRecord);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord maintenanceRecord) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(maintenanceRecord, "maintenanceMoment", "status", "inspectionDueDate", "estimatedCost", "notes", "draftMode");
+		dataset.put("maintenanceMoment", maintenanceRecord.getMaintenanceMoment());
+		dataset.put("status", maintenanceRecord.getStatus());
+		dataset.put("inspectionDueDate", maintenanceRecord.getInspectionDueDate());
+		dataset.put("estimatedCost", maintenanceRecord.getEstimatedCost());
+		dataset.put("notes", maintenanceRecord.getNotes());
+
+		super.getResponse().addData(dataset);
+	}
 
 }
