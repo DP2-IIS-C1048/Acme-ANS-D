2eb75f4e1168f2c156879e11b4ef6a9878d99fcf
Marco2026 <marherluj@alum.us.es>
2025-04-13 14:57:20 +0200

    Create service basic structure

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordCreateService.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordCreateService.java
@@ -1,6 +1,63 @@
+
 package acme.features.technician.maintenanceRecord;
 
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class TechnicianMaintenanceRecordCreateService {
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.components.ExchangeRate;
+import acme.entities.maintenance.MaintenanceRecord;
+import acme.realms.technician.Technician;
+
+@GuiService
+public class TechnicianMaintenanceRecordCreateService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		MaintenanceRecord maintenanceRecord;
+		int maintenanceRecordId;
+
+		maintenanceRecordId = super.getRequest().getData("id", int.class);
+		maintenanceRecord = this.repository.findMaintenanceRecordById(maintenanceRecordId);
+
+		super.getBuffer().addData(maintenanceRecord);
+	}
+
+	@Override
+	public void bind(final MaintenanceRecord maintenanceRecord) {
+		super.bindObject(maintenanceRecord, "maintenanceMoment", "status", "inspectionDueDate", "estimatedCost", "notes");
+	}
+
+	@Override
+	public void validate(final MaintenanceRecord maintenanceRecord) {
+		{
+			boolean validCurrency = ExchangeRate.isValidCurrency(maintenanceRecord.getEstimatedCost().getCurrency());
+			super.state(validCurrency, "cost", "acme.validation.currency.message");
+		}
+	}
+
+	@Override
+	public void perform(final MaintenanceRecord maintenanceRecord) {
+		this.repository.save(maintenanceRecord);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord maintenanceRecord) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(maintenanceRecord, "maintenanceMoment", "status", "inspectionDueDate", "estimatedCost", "notes", "draftMode");
+
+		super.getResponse().addData(dataset);
+	}
 
 }
