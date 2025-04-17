fffde92b4d4ea6290d658856c59eadf061a0c1bc
Marco2026 <marherluj@alum.us.es>
2025-04-17 21:23:19 +0200

    added queries to retrieve aircrafts

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordRepository.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
+import acme.entities.aircraft.Aircraft;
 import acme.entities.maintenance.MaintenanceRecord;
 
 @Repository
@@ -17,5 +18,11 @@ public interface TechnicianMaintenanceRecordRepository extends AbstractRepositor
 
 	@Query("select mr from MaintenanceRecord mr where mr.technician.id = :id")
 	Collection<MaintenanceRecord> findMaintenanceRecordsByTechnicianId(int id);
+
+	@Query("select a from Aircraft a")
+	Collection<Aircraft> findAllAircrafts();
+
+	@Query("select a from Aircraft a where a.id = :id")
+	Aircraft findAircraftById(int id);
 
 }
