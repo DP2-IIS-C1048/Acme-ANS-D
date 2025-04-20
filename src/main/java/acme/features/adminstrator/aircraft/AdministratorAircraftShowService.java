2de1112ce79ffe97448ee8042b7f606c9ba9da95
guigonvaz <guigonvaz@alum.us.es>
2025-04-20 18:41:21 +0200

    Tarea completada

---
+++ src/main/java/acme/features/adminstrator/aircraft/AdministratorAircraftShowService.java
@@ -0,0 +1,63 @@
+
+package acme.features.adminstrator.aircraft;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.aircraft.Aircraft;
+import acme.entities.airline.Airline;
+
+@GuiService
+public class AdministratorAircraftShowService extends AbstractGuiService<Administrator, Aircraft> {
+
+	@Autowired
+	private AdministratorAircraftRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Aircraft aircraft;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		aircraft = this.repository.findAircraftById(id);
+
+		super.getBuffer().addData(aircraft);
+	}
+
+	@Override
+	public void unbind(final Aircraft aircraft) {
+		Dataset dataset;
+		Collection<Airline> airlines;
+		SelectChoices choices;
+		Airline selectedAirline;
+
+		airlines = this.repository.findAllAirlines();
+		selectedAirline = aircraft.getAirline();
+
+		if (selectedAirline != null && !airlines.contains(selectedAirline)) {
+			airlines = new ArrayList<>(airlines);
+			airlines.add(selectedAirline);
+		}
+
+		choices = SelectChoices.from(airlines, "iataCode", selectedAirline);
+
+		dataset = super.unbindObject(aircraft, "model", "registrationNumber", "capacity", "cargoWeight", "status", "details");
+		dataset.put("airline", choices.getSelected().getKey());
+		dataset.put("airlines", choices);
+
+		super.getResponse().addData(dataset);
+	}
+}
---
+++ src/main/resources/WEB-INF/views/administrator/aircraft/form.jsp
@@ -0,0 +1,27 @@
+<%@page%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://acme-framework.org/"%>
+
+<acme:form> 
+
+	<acme:input-textbox code="administrator.aircraft.form.label.model" path="model"/>
+	<acme:input-textbox code="administrator.aircraft.form.label.registrationNumber" path="registrationNumber"/>
+	<acme:input-integer code="administrator.aircraft.form.label.capacity" path="capacity"/>
+	<acme:input-integer code="administrator.aircraft.form.label.cargoWeight" path="cargoWeight"/>
+	<acme:input-textbox code="administrator.aircraft.form.label.status" path="status" placeholder="administrator.aircraft.form.placeholder.status"/>
+	<acme:input-textarea code="administrator.aircraft.form.label.details" path="details"/>
+	<acme:input-select code="administrator.aircraft.form.label.airline" path="airline" choices="${airlines}"/>
+
+	<acme:input-checkbox code="administrator.aircraft.form.label.confirmation" path="confirmation"/>
+	
+	<jstl:choose>	 
+		<jstl:when test="${acme:anyOf(_command, 'show|update|disable')}">
+			<acme:submit code="administrator.aircraft.form.button.update" action="/administrator/aircraft/update"/>
+			<acme:submit code="administrator.aircraft.form.button.disable" action="/administrator/aircraft/disable"/>
+		</jstl:when>
+		<jstl:when test="${_command == 'create'}">
+			<acme:submit code="administrator.aircraft.form.button.create" action="/administrator/aircraft/create"/>
+		</jstl:when>		
+	</jstl:choose>
+</acme:form>
