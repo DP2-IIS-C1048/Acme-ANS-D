785db66bd79a9bf57485d72b3a5c4ddf539eb0bf
Marco2026 <marherluj@alum.us.es>
2025-04-29 11:32:39 +0200

    feat: added query to fetch tasks related to a maintenanceRecord

--- src/main/java/acme/features/technician/involves/TechnicianInvolvesRepository.java
+++ src/main/java/acme/features/technician/involves/TechnicianInvolvesRepository.java
@@ -1,11 +1,18 @@
 
 package acme.features.technician.involves;
 
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
+import acme.entities.maintenance.Task;
 
 @Repository
 public interface TechnicianInvolvesRepository extends AbstractRepository {
+
+	@Query("select i.task from Involves i where i.maintenanceRecord.id = :id")
+	Collection<Task> findTasksByMaintenanceRecordId(int id);
 
 }
