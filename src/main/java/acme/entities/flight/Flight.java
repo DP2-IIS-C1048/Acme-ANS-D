df9a70b64acd426d91a017453295cb6b0adef088
DAVID CHAVES <davchacum@alum.us.es>
2025-05-25 01:43:00 +0200

    Cambios descartados sin querer

--- src/main/java/acme/entities/flight/Flight.java
+++ src/main/java/acme/entities/flight/Flight.java
@@ -4,7 +4,9 @@ package acme.entities.flight;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.Valid;
 
@@ -24,6 +26,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "draftMode")
+})
 public class Flight extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
